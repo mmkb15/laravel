b220d793955950cb382cde6b9f27f37242af4482
mmkb15 <m49966781@gmail.com>
2026-08-30 13:23:22 +0600

    first laravel

---
+++ lara-herd/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+// Route::get('/', function () {
+//     return view('layouts.app');
+// });
+Route::get('/price', function () {
+    return view('pages.price');
+})->name('pricing');
+
+Route::get('/compare', function () {
+    return view('pages.compare');
+})->name('compare-plan');
